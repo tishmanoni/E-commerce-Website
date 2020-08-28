d319a1a0791db6573d53cc220cd462d90de37cc3
tishmanoni <66375712+tishmanoni@users.noreply.github.com>
2020-08-28 16:12:51 +0100

    migrated

--- myshop/myshop/settings.py
+++ myshop/myshop/settings.py
@@ -76,14 +76,11 @@ WSGI_APPLICATION = 'myshop.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'hiitplc',
-        'USER': 'postgres',
-        'PASSWORD': 'mide',
-
-        
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
