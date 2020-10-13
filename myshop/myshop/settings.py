43daec31df97f1e438ebe4697ecc3b592b7b732d
tishmanoni <66375712+tishmanoni@users.noreply.github.com>
2020-10-13 14:09:49 +0100

    changes

--- myshop/myshop/settings.py
+++ myshop/myshop/settings.py
@@ -77,6 +77,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.locale.LocaleMiddleware',
+    
 ]
 
 ROOT_URLCONF = 'myshop.urls'
